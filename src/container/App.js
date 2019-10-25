48c0be7766b9e44375e62741770923eecb223588
uwem udoh <slimudoh@yahoo.com>
2019-10-25 12:06:36 +0100

    single css file'

--- src/container/App.js
+++ src/container/App.js
@@ -1,12 +1,6 @@
 import React from "react";
 import "../assets/styles/reset.scss";
-import "../assets/styles/header.scss";
-import "../assets/styles/sidebar.scss";
-import "../assets/styles/container.scss";
-import "../assets/styles/footer.scss";
-import "../assets/styles/banner.scss";
-import "../assets/styles/device.scss";
-import "../assets/styles/accordion.scss";
+import "../assets/styles/app.scss";
 import Layout from "../components/layout/Layout";
 
 function App() {
