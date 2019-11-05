236bfb961f3b2261a1e1a6c0afaf269e9b800a88
uwem udoh <slimudoh@yahoo.com>
2019-11-05 15:06:37 +0100

    added input logic in hader

--- src/components/content/Header.js
+++ src/components/content/Header.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Header() {
+  const iframeUrl = useRef("null");
+
+  const sendUrl = () => {
+    const inputValue = iframeUrl.current.value;
+  };
+
   return (
     <div className="header">
       <div className="header__logo">RECTANGLE</div>
       <div className="header__search">
         <form>
-          <input type="text" placeholder="Enter url and hit enter" />
+          <input
+            type="text"
+            placeholder="Enter url and hit enter"
+            onChange={sendUrl}
+            ref={iframeUrl}
+          />
         </form>
       </div>
     </div>
