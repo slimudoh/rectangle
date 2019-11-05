24dd2ebe76c7a82369c9552c697efbf25007db63
uwem udoh <slimudoh@yahoo.com>
2019-11-05 15:36:43 +0100

    updated devices

--- src/components/content/Container.js
+++ src/components/content/Container.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Iphone from "../devices/Iphone";
 import Android from "../devices/Android";
 import Windows from "../devices/Windows";
 
 function Header() {
+  const [iphone, setIphone] = useState(false);
+  const [android, setAndroid] = useState(true);
+  const [windows, setWindows] = useState(false);
+
+  const showIphone = () => {
+    setIphone(true);
+    setAndroid(false);
+    setWindows(false);
+  };
+
+  const showAndroid = () => {
+    setIphone(false);
+    setAndroid(true);
+    setWindows(false);
+  };
+
+  const showWindows = () => {
+    setIphone(false);
+    setAndroid(false);
+    setWindows(true);
+  };
+
   return (
     <div className="container">
       <div className="container__content">
-        <Iphone />
-        <Android />
-        <Windows />
+        {iphone ? <Iphone /> : null}
+        {android ? <Android /> : null}
+        {windows ? <Windows /> : null}
       </div>
     </div>
   );
--- src/components/devices/Android.js
+++ src/components/devices/Android.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Device from "../content/Device";
-import iphoneDetails from "../../util/iphone";
+import androidDetails from "../../util/android";
 
 function Android() {
   return (
     <div className="phones">
       <div className="phones__container">
-        {iphoneDetails.map((item, index) => (
+        {androidDetails.map((item, index) => (
           <Device
             dimensions={item.dimensions}
             name={item.name}
